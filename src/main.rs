21a1d05615f99b628ec2c244d9818deaadb29415
Tsuyoshi Nakashima <newnakashima2014@gmail.com>
2023-10-06 06:56:13 +0000

    feat: ゲームのステートを構造体にする

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,50 @@
-use std::io::{stdin, stdout, Write};
+use std::{io::{stdin, stdout, Write}, process::exit};
+use std::cmp;
 use rand::Rng;
+
+struct GameState {
+    hero: Hero,
+    current_monster: Monster,
+}
+
 fn main() {
-    let mut message = String::from("Hi!");
-    let mut current_monster: Monster = Monster::new("", 0, 0, 0);
-    let mut hero: Hero = Hero::new("勇者", 100, 10, 5);
+    let current_monster: Monster = Monster::new("", 0, 0, 0);
+    let hero: Hero = Hero::new("勇者", 100, 10, 5);
+
+    let mut game_state = GameState {
+        hero,
+        current_monster,
+    };
+
     loop {
-        game_loop(&mut message, &mut hero, &mut current_monster);
+        game_loop(&mut game_state);
     }
 }
 
 // ゲームのメインループ
-fn game_loop(message: &mut String, hero: &mut Hero, current_montser: &mut Monster) {
+fn game_loop(game_state: &mut GameState) {
     // モンスターの一覧
     let monsters = monsters();
-    let monster_new_generated = current_montser.hp <= 0;
-    // モンスターがhpが02かどうか
+    let monster_new_generated = game_state.current_monster.hp == 0;
+    // モンスターがhpが0かどうか
     if monster_new_generated {
         // モンスターをランダムで選択して生成
         let mut rng = rand::thread_rng();
         let n = rng.gen_range(0..monsters.len());
-        *current_montser = monsters[n].clone();
-        *message = format!("{}が現れた！", current_montser.name);
+        game_state.current_monster = monsters[n].clone();
+        println!("{}が現れた！", game_state.current_monster.name);
     } else {
         // モンスターが生きている場合はモンスターの情報を表示
-        *message = format!("{}のHP: {}", current_montser.name, current_montser.hp);
+        println!("{}のHP: {}", game_state.current_monster.name, game_state.current_monster.hp);
     }
-
-    print!("{}\n", message);
 
     // ユーザーの入力を促すプロンプト
     print!("どうする？\n{}: 攻撃\n{}: 防御\n{}: 逃げる\n\n{}のHP: {}\n>",
         1,
         2,
         3,
-        hero.name,
-        hero.hp
+        game_state.hero.name,
+        game_state.hero.hp
     );
 
     // ユーザーの入力を受け取る
@@ -46,27 +56,35 @@ fn game_loop(message: &mut String, hero: &mut Hero, current_montser: &mut Monste
         Ok(_) => {
             match input.trim().parse::<i32>().unwrap() {
                 1 => {
-                    print!("{}の攻撃！\n", hero.name);
-                    hero.attack_to(current_montser);
-                    if current_montser.hp <= 0 {
-                        println!("{}を殺した", current_montser.name);
+                    print!("{}の攻撃！\n", game_state.hero.name);
+                    game_state.hero.attack_to(&mut game_state.current_monster);
+                    if game_state.current_monster.hp == 0 {
+                        println!("{}を殺した", game_state.current_monster.name);
                         return;
                     } else {
-                        current_montser.attack_to(hero);
-                        if hero.hp == 0 {
-                            println!("{}は死んでしまった...", hero.name);
+                        println!("{}の攻撃！", game_state.current_monster.name);
+                        game_state.current_monster.attack_to(&mut game_state.hero);
+                        if game_state.hero.hp == 0 {
+                            println!("{}は死んだ", game_state.hero.name);
+                            exit(0);
                         }
                     }
                 },
                 2 => {
-                    hero.defense *= 2;
+                    game_state.hero.defense *= 2;
                 },
                 3 => {
-                    *message = format!("{}は逃げ出した！", hero.name);
-                    current_montser.hp = 0;
+                    println!("負け犬{}は逃げ出した！", game_state.hero.name);
+                    game_state.current_monster.hp = 0;
                 },
                 _ => {
-                    panic!("入力エラー");
+                    println!("コマンドミス！");
+                    println!("{}の攻撃！", game_state.current_monster.name);
+                    game_state.current_monster.attack_to(&mut game_state.hero);
+                    if game_state.hero.hp == 0 {
+                        println!("{}は死んだ", game_state.hero.name);
+                        exit(0);
+                    }
                 }
             }
         },
@@ -74,17 +92,19 @@ fn game_loop(message: &mut String, hero: &mut Hero, current_montser: &mut Monste
             panic!("入力エラー");
         }
     }
-
-    *message = input;
 }
 
 fn monsters() -> Vec<Monster> {
-    let monster_a = Monster::new("ゴブリン", 20, 5, 3);
-    let monster_b = Monster::new("スライム", 15, 4, 4);
+    let goblin = Monster::new("ゴブリン", 20, 5, 3);
+    let slime = Monster::new("スライム", 15, 4, 4);
+    let dragon = Monster::new("ドラゴン", 50, 10, 10);
+    let king_devil = Monster::new("魔王", 1000, 100, 20);
 
     return vec![
-        monster_a,
-        monster_b,
+        goblin,
+        slime,
+        dragon,
+        king_devil,
     ];
 }
 
@@ -117,7 +137,13 @@ trait Attackable {
             println!("{}はダメージを受けていない！", target.name());
         }
     }
-    fn damaged(&mut self, damage: i32);
+    fn damaged(&mut self, damage: i32) {
+        print!("{}は{}のダメージを受けた！\n", self.name(), damage);
+        self.set_hp(cmp::max(self.get_hp() - damage, 0));
+    }
+
+    fn set_hp(&mut self, hp: i32);
+    fn get_hp(&self) -> i32;
 }
 
 impl Monster {
@@ -151,12 +177,15 @@ impl Attackable for Monster {
         self.defense
     }
 
-    fn damaged(&mut self, damage: i32) {
-        print!("{}は{}のダメージを受けた！\n", self.name, damage);
-        self.hp -= damage;
-    }
     fn name(&self) -> String {
         self.name.clone()
+    }
+
+    fn get_hp(&self) -> i32 {
+        self.hp
+    }
+    fn set_hp(&mut self, hp: i32) {
+        self.hp = hp;
     }
 }
 
@@ -169,11 +198,14 @@ impl Attackable for Hero {
         self.defense
     }
 
-    fn damaged(&mut self, damage: i32) {
-        print!("{}は{}のダメージを受けた！\n", self.name, damage);
-        self.hp -= damage;
-    }
     fn name(&self) -> String {
         self.name.clone()
+    }
+
+    fn get_hp(&self) -> i32 {
+        self.hp
+    }
+    fn set_hp(&mut self, hp: i32) {
+        self.hp = hp;
     }
 }
